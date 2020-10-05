c9947b91ae9d6c863a4883bade5e3e33ad6f6c84
Gladstone Moises Arantes Junior <glads@mbp-de-usuario.lan>
2020-10-05 12:55:04 -0300

    Completando limpeza de lixo no código

--- back-blockchain/test/TestNotarizer.sol
+++ back-blockchain/test/TestNotarizer.sol
@@ -2,7 +2,6 @@ pragma solidity >=0.4.25 <0.7.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/Notarizer.sol";
 import "../contracts/RBBRegistry.sol";
 import "../contracts/Notarizer.sol";
 
